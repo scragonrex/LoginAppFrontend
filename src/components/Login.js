f26335414ab33f4aa0b3d7370edaaa8d564f2630
scragonrex <scragondragon@gmail.com>
2022-12-26 16:03:33 +0530

    alert functionality added and validation also

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,13 @@
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { Button, FormControl, IconButton, InputAdornment, InputLabel, LinearProgress, OutlinedInput, TextField, Typography } from '@mui/material'
+import { Alert, Button, FormControl, FormHelperText, IconButton, InputAdornment, InputLabel, LinearProgress, OutlinedInput, Snackbar, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useHistory } from "react-router-dom";
 
-const Login = ({setLoginUser})=>{
+const Login = ({ setLoginUser }) => {
+  const [userNotReg, setUserNotReg] = useState(false)
+  const [invalidPass, setinvalidPass] = useState(false)
   const [open, setOpen] = useState(false);
   const history = useHistory();
   const [showPassword, setShowPassword] = React.useState(false);
@@ -16,70 +18,85 @@ const Login = ({setLoginUser})=>{
     event.preventDefault();
   };
   const [user, setUser] = useState({
-    email:"",
-    password:""
+    email: "",
+    password: ""
   })
 
-  const handleChange = (e)=>{
-    const {name ,value} = e.target
+  const handleChange = (e) => {
+    const { name, value } = e.target
     setUser(
       {
         ...user,
-          [name]:value
+        [name]: value
       }
     )
   }
 
-  const handleClick =()=>{
+  const handleClick = () => {
     history.push("/register");
     // window.location.reload(false);
   }
-  
-  const login=()=>{
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setUserNotReg(false);
+  };
+  const login = () => {
     setOpen(true);
-    axios.post("https://loginappbackend.onrender.com/login",user)
-    .then(res=>{alert(res.data.message)
-          setLoginUser(res.data.user)
-          history.push("/")
-          setOpen(false)
-        })
+    axios.post("https://loginappbackend.onrender.com/login", user)
+      .then(res => {
+        let mes = res.data.message;
+        if (mes === "Invalid Password")
+          setinvalidPass(true);
+        if (mes === "User not registered")
+          setUserNotReg(true);
+        setLoginUser(res.data.user)
+        history.push("/")
+        setOpen(false)
+      })
   }
   return (
-    <Box width='100vw' height='100vh' m={0} p={0} sx={{display:'flex', flexDirection:'column'}}>
-    { open&&<Box m={0} sx={{ width: '100%' }}>
-    <LinearProgress sx={{height:'5px'}}/>
-  </Box>}
-    <Box backgroundColor='white' borderRadius={3} sx={{width:'20rem', height:'28rem',p:3,boxShadow:'0px 0px 6px 0px #dee2e8', margin:'auto'}}>
-      <Typography variant='h4' component='div' textAlign='center'>Login</Typography>
-      <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
-        <TextField name='email' value={user.email}  onChange={handleChange} id="outlined-basic" label="Email" variant="outlined" sx={{margin:'1rem 3rem'}}/>
-        <FormControl sx={{margin:'1rem 3rem'}} variant="outlined">
-          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
-          <OutlinedInput
-            name='password'
-            value={user.password} onChange={handleChange} 
-            id="outlined-adornment-password"
-            type={showPassword ? 'text' : 'password'}
-            endAdornment={
-              <InputAdornment position="end">
-                <IconButton
-                  aria-label="toggle password visibility"
-                  onClick={handleClickShowPassword}
-                  onMouseDown={handleMouseDownPassword}
-                  edge="end"
+    <Box width='100vw' height='100vh' m={0} p={0} sx={{ display: 'flex', flexDirection: 'column' }}>
+      {open && <Box m={0} sx={{ width: '100%' }}> <LinearProgress sx={{ height: '5px' }} /></Box>}
+      <Snackbar open={userNotReg} autoHideDuration={2000} anchorOrigin={{ vertical: 'top', horizontal:   'center' }}    onClose={handleClose}>
+        <Alert onClose={handleClose} severity="error">User not registered!</Alert>
+      </Snackbar>
+      <Box backgroundColor='white' borderRadius={3} sx={{ width: '20rem', height: '28rem', p: 3, boxShadow: '0px 0px 6px 0px #dee2e8', margin: 'auto' }}>
+        <Typography variant='h4' component='div' textAlign='center'>Login</Typography>
+        <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
+          <TextField name='email' value={user.email} onChange={handleChange} id="outlined-basic" label="Email" variant="outlined" sx={{ margin: '1rem 3rem' }} />
+          <FormControl error={invalidPass} sx={{ margin: '1rem 3rem' }} variant="outlined" >
+            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+            <OutlinedInput
+              name='password'
+              value={user.password} onChange={handleChange}
+              id="outlined-adornment-password"
+
+              type={showPassword ? 'text' : 'password'}
+              endAdornment={
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    edge="end"
                   >
-                  {showPassword ? <Visibility /> : <VisibilityOff />}
-                </IconButton>
-              </InputAdornment>
-            }
-            label="Password"
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              }
+              label="Password"
             />
-        </FormControl>
-        <Button variant="contained" sx={{margin:'1rem 5.5rem'}} onClick={login}>Login</Button>
-        <Button variant="contained" sx={{margin:'1rem 5.5rem'}} onClick={handleClick}>Register</Button>
+            {invalidPass && <FormHelperText>Invalid password</FormHelperText>}
+          </FormControl>
+          <Button variant="contained" sx={{ margin: '1rem 5.5rem' }} onClick={login}>Login</Button>
+          <Button variant="contained" sx={{ margin: '1rem 5.5rem' }} onClick={handleClick}>Register</Button>
+        </Box>
+      </Box>
     </Box>
-    </Box>
-</Box>
   )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,15 @@
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { Button, FormControl, IconButton, Input, InputAdornment, InputLabel, LinearProgress, OutlinedInput, TextField, Typography } from '@mui/material'
+import { Alert, Button, FormControl, FormHelperText, IconButton, Input, InputAdornment, InputLabel, LinearProgress, OutlinedInput, Snackbar, TextField, Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import axios from 'axios';
 import React, { useState } from 'react'
 import { useHistory } from "react-router-dom";
 
 const Register = ()=>{
+  const [invalidIn, setInvalidIn] = useState(false);
+  const [incPass, setIncPass] = useState(false);
+  const [loginSuc, setLoginSuc] = useState(false);
+  const [userAlready, setUserAlready] = useState(false);
   const [open, setOpen] = useState(false);
   const history = useHistory();
   const [showPassword, setShowPassword] = React.useState(false);
@@ -33,16 +37,29 @@ const Register = ()=>{
   const register=()=>{
     setOpen(true);
     const {name, email, password, confirmPassword}=user;
-    if(name && email && password && (password === confirmPassword))
-    {
-      axios.post("https://loginappbackend.onrender.com/register", user)
-      .then(res=>{alert(res.data.message)
-            setOpen(false)
-            handleClick()})
-     
+    if(name && email && password)
+    { if(password === confirmPassword)
+      {
+        setIncPass(false);
+        axios.post("https://loginappbackend.onrender.com/register", user)
+        .then(res=>{
+            let mes = res.data.message;
+            if(mes === "Registered Successfully")
+            setLoginSuc(true);
+            if(mes === "User already registered. Please login now")
+            setUserAlready(true);
+            setOpen(false);
+            handleClick();
+          })
+      }
+      else
+      {
+        setIncPass(true);
+        setOpen(false);
+      }
     }
     else{
-      alert("Invalid input");
+      setInvalidIn(true);
       setOpen(false);
     }
   }
@@ -51,16 +68,42 @@ const Register = ()=>{
     history.push("/login");
     // window.location.reload(false);
   }
-  
+  const handleClose1 = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setUserAlready(false);
+  };
+  const handleClose2 = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setLoginSuc(false);
+  };
+  const handleClose3 = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setInvalidIn(false);
+  };
   return (
     <Box width='100vw' height='100vh' m={0} p={0} sx={{display:'flex', flexDirection:'column'}}>
-    { open&&<Box m={0} sx={{ width: '100%' }}>
+    {open &&  <Box m={0} sx={{ width: '100%' }}>
     <LinearProgress sx={{height:'5px'}} />
   </Box>}
+  <Snackbar open={userAlready} autoHideDuration={2000} anchorOrigin={{ vertical: 'top', horizontal:            'center' }}  onClose={handleClose1}>
+        <Alert onClose={handleClose1} severity="info">User already registered!. Please Login</Alert>
+  </Snackbar>
+  <Snackbar open={loginSuc} autoHideDuration={2000} anchorOrigin={{ vertical: 'top', horizontal:            'center' }}  onClose={handleClose2}>
+        <Alert onClose={handleClose2} severity="success">Registered sucessfully!</Alert>
+  </Snackbar>
+  <Snackbar open={invalidIn} autoHideDuration={2000} anchorOrigin={{ vertical: 'top', horizontal:            'center' }}  onClose={handleClose3}>
+        <Alert onClose={handleClose3} severity="error">Invalid input!</Alert>
+  </Snackbar>
     <Box backgroundColor='white'  borderRadius={3} sx={{width:'20rem', height:'33rem',p:3, boxShadow:'0px 0px 4px 0px #dee2e8', margin:'auto'}}>
       <Typography variant='h4' component='div' textAlign='center'>Register</Typography>
       <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center'}}>
-        <TextField name='name' value={user.name}  onChange={handleChange} id="name" label="Username" variant="outlined" sx={{margin:'1rem 1.5rem'}}/>
+        <TextField name='name' value={user.name}  onChange={handleChange} id="name" label="Username" variant="outlined" error={false} sx={{margin:'1rem 1.5rem'}}/>
         <TextField name='email' value={user.email}  onChange={handleChange} id="email" label="Email" variant="outlined" sx={{margin:'1rem 1.5rem'}}/>
         <FormControl sx={{margin:'1rem 1.5rem'}} variant="outlined">
           <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
@@ -84,7 +127,7 @@ const Register = ()=>{
             label="Password"
           />
         </FormControl>
-        <FormControl sx={{margin:'1rem 1.5rem'}} variant="standard">
+        <FormControl sx={{margin:'1rem 1.5rem'}} variant="standard" error={incPass}>
           <InputLabel htmlFor="standard-adornment-password">Confirm Password</InputLabel>
           <Input
             name='confirmPassword'
@@ -103,6 +146,7 @@ const Register = ()=>{
               </InputAdornment>
             }
           />
+          {incPass && <FormHelperText>Invalid password</FormHelperText>}
         </FormControl>
         <Button variant="contained" sx={{margin:'0.5rem 5.5rem'}} onClick={register}>Register</Button>
         <Typography variant='h6' textAlign='center'>or</Typography>
