36d46a5f06ab229436ec0ac0e0277bd35e66fdf7
scragonrex <scragondragon@gmail.com>
2022-12-27 21:26:51 +0530

    Added welcome gif

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,11 @@
 import { Typography } from '@mui/material'
 import { Box } from '@mui/system'
 import React from 'react'
-
+import welcomeGif from './welcome.gif'
 const Homepage = ({userData})=>{
   return (
     <Box backgroundColor='white'  borderRadius={3} sx={{width:'400px', height:'300px', p:3, boxShadow:'0px 0px 4px 0px #dee2e8', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+      <img src={welcomeGif} alt='Welcome page'/>
       <Typography variant='h5'> Welcome {userData.name}</Typography>
       <Typography variant='h5'>Your Email is {userData.email}</Typography>
     </Box>
